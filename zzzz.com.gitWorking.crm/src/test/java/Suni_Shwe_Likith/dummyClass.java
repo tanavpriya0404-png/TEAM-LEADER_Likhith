7bbf49a574a8961dbba65eb75c4f9013d48f9960
Admin <Admin@LAPTOP-LNH5DL0K>
2026-01-17 13:25:57 +0530

    pom.xml and element added in dummy class

--- zzzz.com.gitWorking.crm/src/test/java/Suni_Shwe_Likith/dummyClass.java
+++ zzzz.com.gitWorking.crm/src/test/java/Suni_Shwe_Likith/dummyClass.java
@@ -1,10 +1,18 @@
 package Suni_Shwe_Likith;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class dummyClass {
 public static void main(String[] args) {
 	
-System.out.println();
-
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://demoapps.qspiders.com/ui?scenario=1");
+	driver.findElement(By.name("name")).sendKeys("Likhith");
+	
+	
+	
 }
 }
 
